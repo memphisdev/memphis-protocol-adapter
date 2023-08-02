c9fb532912b892c8161dbf7fe3ab56345f5b5ff8
g41797 <g41797@gmail.com>
2023-08-02 14:00:03 +0300

    Replace viper with github.com/tkanos/gonfig

--- pkg/adapter/config.go
+++ pkg/adapter/config.go
@@ -1,37 +1,35 @@
 package adapter
 
 import (
+	"path/filepath"
+	"strings"
+
 	"github.com/g41797/sputnik"
-	"github.com/spf13/viper"
+	"github.com/tkanos/gonfig"
 )
 
-// ConfigFactory returns implementation of sputnik.ConfFactory based on github.com/spf13/viper
+// ConfigFactory returns implementation of sputnik.ConfFactory based on github.com/tkanos/gonfig
 // - JSON format of config files
 // - Automatic matching of environment variables
-// - Env. variable for configuration "example" should have prefix "EXAMPLE_"
+// - Env. variable for configuration "example" and key "kname" should be set in environment as "EXAMPLE_KNAME"
+// - Value in environment automatically overrides value from the file
+// - Temporary implementation github.com/tkanos/gonfig  will be replaced with github.com/g41797/gonfig
 func ConfigFactory(cfPath string) sputnik.ConfFactory {
 	cnf := newConfig(cfPath)
 	return cnf.unmarshal
 }
 
 type config struct {
-	v *viper.Viper
+	confPath string
 }
 
 func newConfig(cfPath string) *config {
-	v := viper.New()
-	v.AddConfigPath(cfPath)
-	v.AutomaticEnv()
-	v.SetConfigType("json")
-	return &config{v: v}
+	return &config{confPath: cfPath}
 }
 
 func (conf *config) unmarshal(confName string, result any) error {
-	conf.v.SetConfigName(confName)
-	conf.v.SetEnvPrefix(confName)
-	err := conf.v.ReadInConfig()
-	if err == nil {
-		err = conf.v.Unmarshal(result)
-	}
+	fPath := filepath.Join(conf.confPath, strings.ToLower(confName))
+	fPath += ".json"
+	err := gonfig.GetConf(fPath, result)
 	return err
 }
--- pkg/adapter/config_test.go
+++ pkg/adapter/config_test.go
@@ -1,6 +1,8 @@
 package adapter_test
 
 import (
+	"os"
+	"strconv"
 	"testing"
 
 	adapter "github.com/memphisdev/memphis-protocol-adapter/pkg/adapter"
@@ -11,27 +13,66 @@ type TestConf struct {
 	SECONDINT   int    `mapstructure:"SECONDINT"`
 }
 
-func TestPrepare(t *testing.T) {
+func TestJSON(t *testing.T) {
 
 	confFolderPath := "./_conf_test/"
 
 	cfact := adapter.ConfigFactory(confFolderPath)
 
-	expected := TestConf{"1.0.3", 300}
+	expected := defaults()
 
 	var tf TestConf
 
 	err := cfact("example", &tf)
 
-	if err != nil {
-		t.Errorf("unmarshal error %v", err)
+	compare(t, err, tf, expected)
+}
+
+func TestENV(t *testing.T) {
+
+	confFolderPath := "./_conf_test/"
+
+	cfact := adapter.ConfigFactory(confFolderPath)
+
+	expected := defaults()
+	expected.SECONDINT = 12345
+
+	os.Setenv("EXAMPLE_FIRSTSTRING", expected.FIRSTSTRING)
+
+	os.Setenv("EXAMPLE_SECONDINT", strconv.Itoa((expected.SECONDINT)))
+
+	secIntString, ok := os.LookupEnv("EXAMPLE_SECONDINT")
+
+	if !ok {
+		t.Errorf("Wrong working with environment")
 	}
 
-	if tf.FIRSTSTRING != expected.FIRSTSTRING {
-		t.Errorf("Expected %s Actual %s", expected.FIRSTSTRING, tf.FIRSTSTRING)
+	if secIntString != "12345" {
+		t.Errorf("Wrong test")
 	}
 
-	if tf.SECONDINT != expected.SECONDINT {
-		t.Errorf("Expected %d Actual %d", expected.SECONDINT, tf.SECONDINT)
+	var tf TestConf
+
+	err := cfact("example", &tf)
+
+	compare(t, err, tf, expected)
+}
+
+func defaults() TestConf {
+	return TestConf{"1.0.3", 300}
+}
+
+func compare(t *testing.T, getErr error, actual TestConf, expected TestConf) {
+
+	if getErr != nil {
+		t.Errorf("unmarshal error %v", getErr)
+	}
+
+	if actual.FIRSTSTRING != expected.FIRSTSTRING {
+		t.Errorf("Expected %s Actual %s", expected.FIRSTSTRING, actual.FIRSTSTRING)
+	}
+
+	if actual.SECONDINT != expected.SECONDINT {
+		t.Errorf("Expected %d Actual %d", expected.SECONDINT, actual.SECONDINT)
 	}
 }
