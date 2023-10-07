102fe334bfac2ea0aefa93d44415dbde2cde426b
g41797 <g41797@gmail.com>
2023-10-07 05:50:57 +0300

    Clarify imports

--- cmd/syslog-e2e/main.go
+++ cmd/syslog-e2e/main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 
+	// sputnik framework:
 	"github.com/g41797/sputnik/sidecar"
 	"github.com/g41797/starter"
 
+	// memphis connector plugin
 	"github.com/memphisdev/memphis-protocol-adapter/pkg/adapter"
 
-	// Attach blocks and plugins to the process:
+	// Attach blocks and plugins to the process via blank imports:
+
+	// 		syslog-adapter:
+	// 			syslogsidecar blocks: receiver|producer|client|consumer
 	_ "github.com/g41797/syslogsidecar"
+
+	// 			memphis syslog plugins
+	// 				msgconsumer
 	_ "github.com/memphisdev/memphis-protocol-adapter/e2e/syslog"
+	//				msgproducer
 	_ "github.com/memphisdev/memphis-protocol-adapter/pkg/syslog"
 )
 
